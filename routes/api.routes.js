a9fbf8a9287c8594b416bbf754da942368213fa7
PrakashRajendran <prprakash2007@gmail.com>
2015-04-03 19:15:42 -0400

    updated files

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -10,41 +10,52 @@
 /*	Import the required node packages for routes	*/
 var express = require('express'),
 	app = express.Router();
-	formidable = require('formidable'), // used for parsing the incoming form
 	http = require('http'), // for http requests
 	mongoose = require('mongoose'),	// use mongoose to connect with MongoDB
 	Directory = mongoose.model("Directory"); // Mongoose Directory Model
 
 /**	
  *	requestType	:	/GET
- *	routeName	:	/browse-directory
+ *	routeName	:	/request-all
  *	description	:	Get all the business directory listings from the database and display them in the browse-directory page
  */
 app.get('/request-all', function(req, res) {
 	Directory.find(function(err, directories) {
-		// render the browse-directory route page with no records
-		res.render('request-all', { directories: JSON.stringify(directories,undefined, 2)});
+		
+		if (isEmpty(directories)) {
+            res.render('request-all', { directories: 'Oops...We couldn\'t able to find the requested business name ' + req.params.name + ' in our database '});
+		}else {
+			// render all the business listings from the database
+			res.render('request-all', { directories: JSON.stringify(directories,undefined, 2)});
+		}
 	});
 });
 
+/**
+ *	functionName	: isEmpty
+ *	description		: It is used to check the object empty or not
+ */
+var isEmpty = function(obj) {
+  return Object.keys(obj).length === 0;
+}
+
 /**	
  *	requestType	:	/GET
- *	paramId		:	id
+ *	paramId		:	name
  *	routeName	:	/request-business/:name
  *	description	:	Find the requested business name and get business listing information
  */
 app.get('/request-business/:name', function (req, res, next) {
-    //store the id from the url in a variable
     
+	//get the business name from the reqest parameters and create the below query
 	var query = { 'businessName' : req.params.name };
 
+	//pass the query object to find the business listing in the databbase
     Directory.findOne(query, function (err, directory) {
         if (err) {
-			console.log(err);
-            res.send('Directory Listing ' + id + ' not found');
+            res.render('request-business', { directory: 'Oops...We couldn\'t able to find the requested business name ' + req.params.name + ' in our database '});
         }
         else {
-			console.log(directory);
             res.render('request-business', { directory: directory});
         }
     });
